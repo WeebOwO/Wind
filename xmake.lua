6d87d5494870efc4d354a425e09c6cf3d9eb3917
WeebOwO <xingyufeng@mail.nankai.edu.cn>
2024-01-29 21:58:34 +0800

    update build script

--- xmake.lua
+++ xmake.lua
@@ -2,9 +2,7 @@ set_project("WindEngine")
 
 add_rules("mode.debug", "mode.release")
 
-add_includedirs("Wind/")
-
-add_includedirs("3rdParty/")
+add_includedirs("Wind/", "3rdParty/")
 
 set_languages("c++20")
 set_runtimes("MD")
